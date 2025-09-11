77433b36aeeb2454953f78b1bb12977e7d6a0128
snowy2002 <3099508313@qq.com>
2025-09-11 18:44:27 +0800

    modify envId

--- themeConfig.mjs
+++ themeConfig.mjs
@@ -368,7 +368,8 @@ export const themeConfig = {
     twikoo: {
       // 必填，若不想使用 CDN，可以使用 pnpm add twikoo 安装并引入
       js: "https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/twikoo/1.6.39/twikoo.all.min.js",
-      envId: "https://twikoo.24811213.xyz/",
+      // envId: "https://twikoo.24811213.xyz/",
+      envId: "https://teal-salamander-81a2f7.netlify.app",
       // 环境地域，默认为 ap-shanghai，腾讯云环境填 ap-shanghai 或 ap-guangzhou；Vercel 环境不填
       region: "",
       lang: "zh-CN",
